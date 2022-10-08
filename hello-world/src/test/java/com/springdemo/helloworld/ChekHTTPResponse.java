a15d27162e79176e073514cacadde35092bc4f36
Andrew Ang <andrewang@Andrews-MacBook-Air.local>
2022-10-08 21:49:20 +1100

    Added student demo and hello world

---
+++ hello-world/src/test/java/com/springdemo/helloworld/ChekHTTPResponse.java
@@ -0,0 +1,32 @@
+package com.springdemo.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest (webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ChekHTTPResponse {
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Test
+    public void shouldPassIfStringMatches() {
+        assertEquals("Hello World from Spring Boot",
+                testRestTemplate.getForObject("http://localhost:"+ port + "/",
+                        String.class));
+    }
+
+    @Test
+    public void checkGoodBye() {
+        assertEquals("Good bye from Spring Boot",
+                testRestTemplate.getForObject("http://localhost:" + port + "/goodbye",
+                        String.class));
+    }
+}
