e8548f84548dc6b9f702c0b11f19852edcd54e28
Andrew Ang <andrewang@Andrews-MacBook-Air.local>
2022-10-09 21:43:33 +1100

    Readded Student

---
+++ StudentDemo/src/test/java/com/andrewang/demo/DemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.andrewang.demo;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled("Integration with postgresSQL database not done")
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
